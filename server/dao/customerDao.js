e206443d2567e9c0b00f71fdfdea6ff41b0df4b0
Shubham-com <66332826+Shubham-com@users.noreply.github.com>
2021-02-28 23:03:16 +0530

    Update customerDao.js

--- server/dao/customerDao.js
+++ server/dao/customerDao.js
@@ -23,6 +23,49 @@ class customerDao {
                     cb(false, ` error in customer Dao readByOrder${err}`)
                 })
             }
+            this.transferMoney = (toCustId, amount, fromCustId, cb) => {
+            models.customer.findAll({
+                where: {
+                    customerid: toCustId
+                }
+            }).then(data => {
+                data[0].currBalance = parseInt(data[0].currBalance) + parseInt(amount);
+                data[0].save().then(data => {
+                    this.updatefromCust(amount, fromCustId, (result, msg) => {
+                        if (result == true) {
+                            cb(true, msg)
+                        } else {
+                            cb(false, msg)
+                        }
+                    })
+                }).catch(err => {
+                    console.log(err);
+                    cb(false, err);
+                })
+            }).catch(err => {
+                console.log(err);
+                cb(false, err);
+            })
+        }
+
+        this.updatefromCust = (amount, fromCustId, cb) => {
+            models.customer.findAll({
+                where: {
+                    customerid: fromCustId
+                }
+            }).then(data => {
+                data[0].currBalance =  parseInt(data[0].currBalance) + parseInt(amount);
+                data[0].save().then(data => {
+                    cb(true, data)
+                }).catch(err => {
+                    console.log(err);
+                    cb(false, err);
+                })
+            }).catch(err => {
+                console.log(err);
+                cb(false, err);
+            })
+        }
     }
 }
 
