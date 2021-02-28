0208d356e7b4255bbe0b7e964f867ef0567f1233
Shubham-com <66332826+Shubham-com@users.noreply.github.com>
2021-02-28 23:05:19 +0530

    Update customerService.js

--- server/service/customerService.js
+++ server/service/customerService.js
@@ -52,4 +52,32 @@ function getTransferToList(req, res) {
 
 }
 
-module.exports = { getAllCustomers, getTransferToList }
+function updateTransferredMoney(req, res) {
+    let customerDao = new CustomerDao();
+    try {
+        let toCustId = req.params.toCustId;
+        let amount = req.params.amount;
+        let fromCustId=req.params.fromCustId;
+        customerDao.transferMoney(toCustId,amount,fromCustId, (status, data) => {
+            if (status == true) {
+                res.json({
+                    status: 200,
+                    msg: data
+                });
+            } else {
+                res.json({
+                    status: 2000,
+                    msg: data
+                });
+            }
+        })
+    } catch (e) {
+        res.json({
+            status: 2000,
+            msg: `Error in customerService getTransferToList ${e}`
+        });
+    }
+
+}
+
+module.exports = { getAllCustomers, getTransferToList,updateTransferredMoney }
